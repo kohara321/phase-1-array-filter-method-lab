50924ed31ff22355c380c1c102f0eaf8f8b79966
Kaegan O'Hara <kohara321@gmail.com>
2022-05-18 10:31:09 -0600

    commit tree 0e79c4cc5ab372d4e4b90fe7f7db9b077910432b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers, sought) {
+    return drivers.filter(
+        match => match.toUpperCase() === sought.toUpperCase()
+    )
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(
+        match => match.toUpperCase().indexOf(string.toUpperCase()) === 0
+    )
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(record => record.name === name)
+}
